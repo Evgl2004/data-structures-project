ec5ef91dec56176302dd3364ece101ac65831962
Andrey Evgl <evgl2004@yandex.ru>
2023-08-22 08:51:54 +0500

    Добавлено решение домашней работы № 4

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 from src.queue import Queue
 
 if __name__ == '__main__':
+    # Создаем пустую очередь
     queue = Queue()
-
-    # Магический метод __str__ возвращает пустую строку
-    assert str(Queue()) == ""
 
     # Добавляем данных в очередь
     queue.enqueue('data1')
     queue.enqueue('data2')
     queue.enqueue('data3')
 
-    # Проверяем очередность хранения данных
-    assert queue.head.data == 'data1'
-    assert queue.head.next_node.data == 'data2'
-    assert queue.tail.data == 'data3'
-    assert queue.tail.next_node is None
-    print(queue.tail.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
-
-    # Проверяем магический метод __str__
-    assert str(queue) == "data1\ndata2\ndata3"
+    assert queue.dequeue() == 'data1'
+    assert queue.dequeue() == 'data2'
+    assert queue.dequeue() == 'data3'
+    assert queue.dequeue() is None
